d2a330128953cf58ae4190ef5be49f51b5c6f0b2
Nicola <npalavec@itba.edu.ar>
2019-10-08 16:10:25 -0300

    Add memory entity

--- features/entities/index.feature
+++ features/entities/index.feature
@@ -5,6 +5,6 @@ Scenario: boxes
   And a box B2 with { "color": "green" }
   And box B2 also has { "color": "blue" }
   Then the document for box B1 at color is "red"
-  And that document at id is 0
+  And that document at id is 1
   And the document for box B2 at color is "blue"
   And the box B3 was deleted
---
+++ src/entities/memory.ts
@@ -0,0 +1,44 @@
+import { Entity, IdOrObject } from './types';
+
+const generate = <T, Tid extends keyof T>(
+  idField: Tid,
+  newId: () => T[Tid],
+) => {
+  const entities: T[] = [];
+
+  const isObj = (v: IdOrObject<T, Tid>): v is T => v && idField in v;
+  type Entries = [keyof T, T[keyof T]][];
+
+  const entityMethods: Entity<T, Tid> = {
+    create: async (record) => {
+      const e = { ...(record as T), [idField]: newId() };
+      entities.push(e);
+      return e;
+    },
+    delete: async (id) => {
+      const entity = await entityMethods.findById(id);
+      if (!entity) return;
+      entities.splice(entities.findIndex(e => e === entity, 1));
+    },
+    findBy: async (record: object) => {
+      const entries = Object.entries(record) as Entries;
+      return entities.find(
+        e =>
+          entries.every(pair => e[pair[0]] === pair[1]) ||
+          (idField in record && e[idField] === (record as any)[idField]),
+      );
+    },
+    findById: async (record) => {
+      const id = isObj(record) ? record[idField] : record;
+      return entityMethods.findBy({ [idField]: id });
+    },
+    update: async (record, update) => {
+      const entity = await entityMethods.findById(record);
+      return Object.assign(entity, update);
+    },
+  };
+
+  return entityMethods;
+};
+
+export default generate;
